d9223e3f21f0ffcf4a05a91257c4db0be5eba633
WanghcPKU <165775424+WanghcPKU@users.noreply.github.com>
2024-07-27 21:25:51 +0800

    Update downsample.r

--- downsample.r
+++ downsample.r
@@ -1,6 +1,7 @@
 df <- readRDS("df.rds")
 df <- subset(df, downsample = 1000) ###总体抽1000
 
+allCells=names(Idents(df))
 allType = levels(Idents(df))
 choose_Cells = unlist(lapply(allType, function(x){
   cgCells = allCells[Idents(df)== x ]
